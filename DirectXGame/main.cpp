e2a954ed732120600bcecca47a98e7b695dd7dc4
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-03-05 00:17:39 +0900

    ウィンドウのサイズ変更を不可に設定

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -23,6 +23,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
 	win->CreateGameWindow();
+	win->SetSizeChangeMode(WinApp::SizeChangeMode::kNone);
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
