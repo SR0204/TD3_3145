0b9f448171bc86d473e694ffc39712d96b0ae4e5
sora_suzuki <k022g0047@g.neec.ac.jp>
2025-03-05 15:01:40 +0900

    ゲームパッドで出来る事を確認できるようにした

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -15,6 +15,10 @@ void Player::Initialize() {
 	textureHandle_ = TextureManager::Load("uvChecker.png"); // テクスチャの読み込み
 	input_ = Input::GetInstance();//入力のインスタンスを取得
 
+	vibrationVal_ = {};
+	deadZone_ = 8000;//デッドゾーン
+	trigerDeadZone_ = 30;//トリガーのデッドゾーン
+
 	hitRad_ = 1.0f;//当たり判定の半径
 	rotateVel_ = {0.02f, 0.02f, 0.02f};//旋回速度
 	moveVel_ = 0.1f;//移動速度
@@ -22,31 +26,135 @@ void Player::Initialize() {
 
 void Player::Update() {
 
+	//コントローラー関係
+	///ーーーここからーーー
+
+	DWORD dwResult = XInputGetState(0, &state_);//コントローラーの状態を取得
+	vibration_.wLeftMotorSpeed = static_cast<WORD>(vibrationVal_.x);  // 左モーターの振動強さ（最大値）
+	vibration_.wRightMotorSpeed = static_cast<WORD>(vibrationVal_.y); // 右モーターの振動強さ（最大値）
+	XInputSetState(0, &vibration_); // コントローラー0のバイブレーションを設定
+	int leftThumbX = state_.Gamepad.sThumbLX;//左スティックのX値
+	int leftThumbY = state_.Gamepad.sThumbLY;//左スティックのY値
+	int rightThumbX = state_.Gamepad.sThumbRX;//右スティックのX値
+	int rightThumbY = state_.Gamepad.sThumbRY;//右スティックのX値
+	BYTE leftTrigger = state_.Gamepad.bLeftTrigger;//左トリガーの値
+	BYTE rightTrigger = state_.Gamepad.bRightTrigger;//右トリガーの値
+
+	// デッドゾーンの適用
+	ApplyDeadZone(leftThumbX);
+	ApplyDeadZone(leftThumbY);
+	ApplyDeadZone(rightThumbX);
+	ApplyDeadZone(rightThumbY);
+	ApplyTriggerDeadZone(leftTrigger);
+	ApplyTriggerDeadZone(rightTrigger);
+
+
+	//コントローラーの状態をImGuiで出力
+	ImGui::Begin("Controller");
+	if (dwResult == ERROR_SUCCESS){
+		// コントローラーが接続出来ている時
+		ImGui::Text("Connect");
+		//ボタン入力を確認
+		if (state_.Gamepad.wButtons & XINPUT_GAMEPAD_A) {
+			ImGui::Text("A Buttun is Push");
+		} else {
+			ImGui::Text("A Buttun is Release");
+		}
+		if (state_.Gamepad.wButtons & XINPUT_GAMEPAD_B) {
+			ImGui::Text("B Buttun is Push");
+		} else {
+			ImGui::Text("B Buttun is Release");
+		}
+		if (state_.Gamepad.wButtons & XINPUT_GAMEPAD_X) {
+			ImGui::Text("X Buttun is Push");
+		} else {
+			ImGui::Text("X Buttun is Release");
+		}
+		if (state_.Gamepad.wButtons & XINPUT_GAMEPAD_Y) {
+			ImGui::Text("Y Buttun is Push");
+		} else {
+			ImGui::Text("Y Buttun is Release");
+		}
+		if (state_.Gamepad.wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER) {
+			ImGui::Text("LS Buttun is Push");
+		} else {
+			ImGui::Text("LS Buttun is Release");
+		}
+		if (state_.Gamepad.wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER) {
+			ImGui::Text("RS Buttun is Push");
+		} else {
+			ImGui::Text("RS Buttun is Release");
+		}
+		if (state_.Gamepad.wButtons & XINPUT_GAMEPAD_START) {
+			ImGui::Text("START Buttun is Push");
+		} else {
+			ImGui::Text("START Buttun is Release");
+		}
+		if (state_.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_UP) {
+			ImGui::Text("UP Buttun is Push");
+		} else {
+			ImGui::Text("UP Buttun is Release");
+		}
+		if (state_.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_DOWN) {
+			ImGui::Text("DOWN Buttun is Push");
+		} else {
+			ImGui::Text("DOWN Buttun is Release");
+		}
+		if (state_.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_RIGHT) {
+			ImGui::Text("RIGHT Buttun is Push");
+		} else {
+			ImGui::Text("RIGHT Buttun is Release");
+		}
+		if (state_.Gamepad.wButtons & XINPUT_GAMEPAD_DPAD_LEFT) {
+			ImGui::Text("LEFT Buttun is Push");
+		} else {
+			ImGui::Text("LEFT Buttun is Release");
+		}
+		ImGui::Text("leftThumb  [%6d,%6d]", leftThumbX, leftThumbY);
+		ImGui::Text("rightThumb [%6d,%6d]", rightThumbX, rightThumbY);
+		ImGui::Text("leftTriger [%3d]", leftTrigger);
+		ImGui::Text("rightTriger[%3d]", rightTrigger);
+		ImGui::Text("DeadZone Max [32767]");
+		ImGui::DragInt("DeadZone", &deadZone_, 1, 0, 32767);
+		ImGui::Text("TriggerDeadZone Max[255]");
+		ImGui::DragInt("TriggerDeadZone", &trigerDeadZone_, 1, 0, 255);
+		ImGui::Text("Vibration Max [65535]");
+		ImGui::DragFloat2("Vibration", &vibrationVal_.x, 100.0f, 0.0f, 65535.0f);
+	} else {
+		// コントローラーが接続出来ていない時
+		ImGui::Text("Unconnect");
+	}
+
+	ImGui::End();
+
+	///ーーーここまでーーー
+
 	const float pi = float(M_PI);
 
 	// ワールド変換データの更新
 	worldTransform_.UpdateMatrix(true);
 
-	//旋回処理[I:上 K:下 J:左 L:右]
+	//旋回処理[I:上 K:下 L:右 J:左 ]
 	if (input_->PushKey(DIK_K)) {
 		worldTransform_.rotation_.x += rotateVel_.x;
 	}
 	if (input_->PushKey(DIK_I)) {
 		worldTransform_.rotation_.x -= rotateVel_.x;
 	}
-	if (input_->PushKey(DIK_J)){
-		worldTransform_.rotation_.y -= rotateVel_.y;
-	}
 	if (input_->PushKey(DIK_L)) {
 		worldTransform_.rotation_.y += rotateVel_.y;
 	}
+	if (input_->PushKey(DIK_J)){
+		worldTransform_.rotation_.y -= rotateVel_.y;
+	}
+	
 
 	//旋回値が|π|を越えたら値を変える
 	if (worldTransform_.rotation_.y >= pi || worldTransform_.rotation_.y <= -pi) {
 		worldTransform_.rotation_.y *= -1;
 	}
 
-	//移動処理(向いてる方向に進む)[W:前進 S:後退 A:左 D:右]
+	//移動処理(向いてる方向に進む)[W:前進 S:後退 D:右 A:左 ]
 	if (input_->PushKey(DIK_W)){
 		//座標に角度と移動速度を乗算した値を足す
 		worldTransform_.translation_.z += cosf(worldTransform_.rotation_.y) * moveVel_;
--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -35,11 +35,37 @@ public://メンバ関数
 	/// <returns>ワールドトランスフォームを取得</returns>
 	const WorldTransform& GetWorldTransform() { return worldTransform_; }
 
+	/// <summary>
+	/// デッドゾーンの処理
+	/// </summary>
+	/// <param name="axis"></param>
+	void ApplyDeadZone(int& axis) {
+		if (axis > -deadZone_ && axis < deadZone_) {
+			axis = 0; // デッドゾーン内の値を無視
+		}
+	}
+
+	/// <summary>
+	/// トリガーのデッドゾーンを適用
+	/// </summary>
+	/// <param name="trigger"></param>
+	void ApplyTriggerDeadZone(BYTE& trigger) {
+		if (trigger < trigerDeadZone_) {
+			trigger = 0;
+		}
+	}
+
 private://メンバ変数
 	WorldTransform worldTransform_;	//ワールド変換データ
 	Model* model_ = nullptr;		//モデル
 	uint32_t textureHandle_;		//テクスチャ
 	Input* input_ = nullptr;		//入力
+
+	XINPUT_STATE state_;         // コントローラーの状態
+	XINPUT_VIBRATION vibration_; // バイブレーション値
+	Vector2 vibrationVal_;//調整用バイブレーション値
+	int deadZone_;//デッドゾーン
+	int trigerDeadZone_;//トリガーのデッドゾーン
 
 	float hitRad_;		//当たり判定の半径
 	Vector3 rotateVel_;	//旋回速度
