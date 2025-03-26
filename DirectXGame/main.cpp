474154cbe906081111f496d1c85e6e500737dcbb
SR0204 <k023g0023@g.neec.ac.jp>
2025-03-26 15:54:14 +0900

    タイトル、クリア、オーバーのシーン切り替えまでやった

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,11 +1,35 @@
 #include "Audio.h"
 #include "AxisIndicator.h"
+#include "Clear.h"
 #include "DirectXCommon.h"
 #include "GameScene.h"
 #include "ImGuiManager.h"
+#include "Over.h"
 #include "PrimitiveDrawer.h"
 #include "TextureManager.h"
 #include "WinApp.h"
+
+GameScene* gameScene = nullptr;
+// TitleScene* titleScene = nullptr;
+Clear* clearScene_ = nullptr;
+Over* overScene_ = nullptr;
+
+// シーン(型)
+enum class Scene {
+	kUnknown = 0,
+
+	kTitle,
+	kGame,
+	kClear,
+	kOver,
+};
+
+// 現在シーン
+Scene scene = Scene::kUnknown;
+
+void ChangeScene();
+void UpdateScene();
+void DrawScene();
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -16,7 +40,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Audio* audio = nullptr;
 	AxisIndicator* axisIndicator = nullptr;
 	PrimitiveDrawer* primitiveDrawer = nullptr;
-	GameScene* gameScene = nullptr;
+	//GameScene* gameScene = nullptr;
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
@@ -62,6 +86,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	gameScene = new GameScene();
 	gameScene->Initialize();
 
+	// タイトル
+	scene = Scene::kTitle;
+	/*titleScene = new TitleScene;
+	titleScene->Initialize();*/
+
 	// メインループ
 	while (true) {
 		// メッセージ処理
@@ -75,6 +104,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		input->Update();
 		// ゲームシーンの毎フレーム処理
 		gameScene->Update();
+
+		// シーン切り替え
+		ChangeScene();
+		// 現在シーン更新
+		UpdateScene();
+
 		// 軸表示の更新
 		axisIndicator->Update();
 		// ImGui受付終了
@@ -84,6 +119,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		dxCommon->PreDraw();
 		// ゲームシーンの描画
 		gameScene->Draw();
+
+		// 現在シーンの描画
+		DrawScene();
+
 		// 軸表示の描画
 		axisIndicator->Draw();
 		// プリミティブ描画のリセット
@@ -96,6 +135,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// 各種解放
 	delete gameScene;
+
+	//delete titleScene;
+
 	// 3Dモデル解放
 	Model::StaticFinalize();
 	audio->Finalize();
@@ -106,4 +148,101 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	win->TerminateGameWindow();
 
 	return 0;
+}
+
+void ChangeScene() {
+	switch (scene) {
+	case Scene::kTitle:
+		//if (titleScene->IsFinished()) {
+		//	// シーン変更
+		//	scene = Scene::kGame;
+		//	// 旧シーンの開放
+		//	delete titleScene;
+		//	titleScene = nullptr;
+		//	// 新シーンの生成と初期化
+		//	gameScene = new GameScene;
+		//	gameScene->Initialize();
+		//}
+		//break;
+	case Scene::kGame:
+		if (gameScene->IsFinished()) {
+			if (gameScene->IsClear() == true) {
+				// シーン変更
+				scene = Scene::kClear;
+				// 旧シーンの開放
+				delete gameScene;
+				gameScene = nullptr;
+				// 新シーンの生成と初期化
+				clearScene_ = new Clear;
+				clearScene_->Initialize();
+			} else {
+				// シーン変更
+				scene = Scene::kOver;
+				// 旧シーンの開放
+				delete gameScene;
+				gameScene = nullptr;
+				// 新シーンの生成と初期化
+				overScene_ = new Over;
+				overScene_->Initialize();
+			}
+		}
+		break;
+	case Scene::kClear:
+		if (clearScene_->IsFinished()) {
+			// シーン変更
+			scene = Scene::kTitle;
+			// 旧シーンの開放
+			delete clearScene_;
+			clearScene_ = nullptr;
+			// 新シーンの生成と初期化
+			/*titleScene = new TitleScene;
+			titleScene->Initialize();*/
+		}
+		break;
+	case Scene::kOver:
+		if (overScene_->IsFinished()) {
+			// シーン変更
+			scene = Scene::kTitle;
+			// 旧シーンの開放
+			delete overScene_;
+			overScene_ = nullptr;
+			// 新シーンの生成と初期化
+			/*titleScene = new TitleScene;
+			titleScene->Initialize();*/
+		}
+		break;
+	}
+}
+
+void UpdateScene() {
+	switch (scene) {
+	case Scene::kTitle:
+		//titleScene->Update();
+		break;
+	case Scene::kGame:
+		gameScene->Update();
+		break;
+	case Scene::kClear:
+		clearScene_->Update();
+		break;
+	case Scene::kOver:
+		overScene_->Update();
+	}
+}
+
+void DrawScene() {
+	switch (scene) {
+	case Scene::kTitle:
+		//titleScene->Draw();
+		break;
+	case Scene::kGame:
+		gameScene->Draw();
+		break;
+	case Scene::kClear:
+		clearScene_->Draw();
+		break;
+	case Scene::kOver:
+		overScene_->Draw();
+		break;
+	}
 }
