faf8a58f0977422c268c662d8e31a6839d0397d1
SR0204 <k023g0023@g.neec.ac.jp>
2025-04-22 16:51:14 +0900

    enemyを少し変えた

--- DirectXGame/Enemy.cpp
+++ DirectXGame/Enemy.cpp
@@ -62,7 +62,7 @@ void Enemy::OnCollosion() { isStop_ = true; }
 
 void Enemy::Move() {
 	MoveVector_.x = (rand() % 100 - 50) / 500.0f; // -0.1 ～ 0.1
-	MoveVector_.y = (rand() % 100 - 50) / 500.0f; // -0.1 ～ 0.1
+	//MoveVector_.y = (rand() % 100 - 50) / 500.0f; // -0.1 ～ 0.1
 	MoveVector_.z = (rand() % 100 - 50) / 500.0f; // -0.1 ～ 0.1
 
 	moveChangeTimer_ = 120; // 60フレームごとに変更
